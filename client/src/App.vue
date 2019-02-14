ed81133cd868a9b1650d9c993c2ac056f8a00061
heavy-matill <heavymatill@gmail.com>
2019-02-14 18:13:22 +0100

    added tabbed view

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,74 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <div>
+      <v-toolbar
+        color="cyan"
+        dark
+        tabs
+      >
+        <!--<v-toolbar-side-icon></v-toolbar-side-icon>-->
+        <v-toolbar-title>Page title</v-toolbar-title>
+<!--
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>-->
+
+        <v-tabs
+          slot="extension"
+          v-model="tabs"
+          centered
+          color="cyan"
+          slider-color="yellow"
+        >
+          <v-tab
+            v-for="name in ['HelloWorld', 'Queue']"
+            :key="name"
+          >
+          {{ name }}
+          </v-tab>
+        </v-tabs>
+      </v-toolbar>
+
+      <v-tabs-items v-model="tabs">
+        <v-tab-item
+          :key="'HelloWorld'"
+        >
+          <v-card flat>
+            <v-card-text><hello-world/></v-card-text>
+          </v-card>
+        </v-tab-item>
+        <v-tab-item
+          :key="'Queue'"
+        >
+          <v-card flat>
+            <v-card-text><queue/></v-card-text>
+          </v-card>
+        </v-tab-item>
+      </v-tabs-items>
+    </div>
+  </v-app>
 </template>
 
 <script>
+import HelloWorld from './Components/HelloWorld'
+import Queue from './Components/Queue'
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      tabs: 1
+    }
+  },
+  components: {
+    HelloWorld,
+    Queue
+  }
 }
 </script>
 
--- client/src/main.js
+++ client/src/main.js
@@ -2,14 +2,18 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  router,
+  // router,
   components: { App },
   template: '<App/>'
 })
